6564c74ac68216a13b6399416bae750e763ab4fe
PerryTham <jirakit.tham@gmail.com>
2024-12-29 19:09:15 -0800

    env.example and requirements.txt

--- currency_tracking.py
+++ currency_tracking.py
@@ -9,15 +9,14 @@ api_key = os.getenv("api_key")
 
 url = "https://api.fxratesapi.com/latest?api_key={api_key}&base=USD&currencies&resolution=1h&amount=1&places=6&format=json"
 
-response = requests.request("GET", url)
+response = requests.request("GET", url) 
 
-# print(response.text)
+# print(response.text) # use this command to see available currency.
 try:
     response = requests.get(url)
     response.raise_for_status()  # Raises an HTTPError for bad responses
     data = response.json()
     usd_to_thb = data['rates']['THB']
-    print(f'Current USD to THB exchange rate: {usd_to_thb}')
 except requests.exceptions.HTTPError as http_err:
     print(f'HTTP error occurred: {http_err}')
 except Exception as err:
@@ -28,7 +27,7 @@ def push_notification(rate):
     notification.notify(
         title="Currency Tracker",
         message=f"Current USD to THB rate: {rate}",
-        timeout=10  # Notification stays for 10 seconds
+        timeout=15  # Notification stays for 15 seconds
     )
 
 push_notification(usd_to_thb)
